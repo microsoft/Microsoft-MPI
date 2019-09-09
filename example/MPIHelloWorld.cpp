cc2cc15c34c344db67f3d9551d26347179dbdf2f
U-REDMOND\hopourre <hopourre@maple1.redmond.corp.microsoft.com>
2019-09-09 15:27:03 -0700

    an example with documentation

---
+++ example/MPIHelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <mpi.h>
+
+
+int main()
+{
+	int my_rank;
+	int world_size;
+
+	MPI_Init(NULL, NULL);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	std::cout << "Hello World from process " << my_rank << " out of " << world_size << " processes!!!" << std::endl;
+
+	MPI_Finalize();
+	return 0;
+}
